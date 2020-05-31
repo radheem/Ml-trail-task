4b834ab8890b518fdf66aeda30ddbb0abefefaca
radheem <radheem98@gmail.com>
2020-05-31 16:21:54 +0500

    completing website backend

--- model_site/api/predictor/views.py
+++ model_site/api/predictor/views.py
@@ -8,10 +8,16 @@ class call_model(APIView):
         if request.method == 'GET':
             # get sound from request
             var = request.GET.get('var')
-            x = np.array(var)
-            x = x.reshape(1,-1)
+            # parsing the list for input features 
+            var = str(var).split(",")
+            # converting list to numpy array to use as input
+            x = np.array(var).astype(np.float)
+            # reshaping input to desirable shape
+            x = x.reshape(1,4)
+            # passing features into model
             prediction = PredictorConfig.model.predict(x)
+            
             # build response
-            response = {'y': prediction}
+            response = {'y': list(prediction)}
             # return response
             return JsonResponse(response)
